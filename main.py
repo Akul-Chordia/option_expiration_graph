d32fcb59cd9d32476e0a7f69d349d2dad291806a
acool8868 <acool8868@gmail.com>
2025-05-10 22:42:18 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import argparse
+import matplotlib.pyplot as plt
+
+positions, strikes, points = [], [], []
+n = int(input("Number of options : "))
+print("Enter : quantity|option_type|strike|price|quantity")
+
+while n>0:
+    cmd = input("> ").split()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("qty" , type=int)
+    parser.add_argument("type" , choices=["call" , "put"])
+    parser.add_argument("strike" , type=float)
+    parser.add_argument("price" , type=float)
+    positions.append(parser.parse_args(cmd))
+    n -= 1
+
+for i in positions:
+    strikes.append(i.strike)
+
+sorted(strikes)
+strikes.append(strikes[-1]+5)
+strikes.append(strikes[0]-5)
+
+strikes = set(strikes)
+
+for strike in strikes:
+    cost = 0.00
+    for position in positions:
+        if position.type == "call":
+            if strike <= position.strike:
+                cost -= position.qty * position.price
+            else:
+                cost += position.qty * (strike - position.strike - position.price)
+        else:
+            if strike >= position.strike:
+                cost -= position.qty * position.price
+            else:
+                cost += position.qty * (position.strike - strike - position.price)
+    points.append((strike,(round(cost,2))))
+
+points.sort()
+
+x_vals = [point[0] for point in points]
+y_vals = [point[1] for point in points]
+
+inner_x = x_vals[1:-1]
+inner_y = y_vals[1:-1]
+
+plt.figure(figsize=(10, 6))
+plt.plot(x_vals, y_vals, linestyle='-', color='blue', label='P&L')
+
+plt.scatter(inner_x, inner_y, color='blue', zorder=5)
+
+plt.axhline(0, color='black', linewidth=0.8, linestyle='--')
+
+plt.title("Options Strategy Payoff Diagram")
+plt.xlabel("Underlying Price at Expiry (Strike)")
+plt.ylabel("Net Profit / Loss")
+plt.grid(True)
+plt.legend()
+plt.show()
+
